5768d121742f1f2854821fead6d94bd98ecd3aae
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-17 17:05:44 +0300

    create a method to display projects in the views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from . models import Project
 
 def home(request):
     title = "Portfolio"
     
     return render(request, 'index.html',{'title':title})
+
+def project(request):
+    projects = Project.objects.all()
+    
+    context = {
+        'projects':projects
+    }
+    
+    return render(request, 'project.html', context)
