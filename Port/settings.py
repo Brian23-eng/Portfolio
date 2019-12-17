6f2112bd827e23b721dbf1288b41551ba823c0f5
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-17 16:35:50 +0300

    create database and connect it to the web app using psycopg2 drive

--- Port/settings.py
+++ Port/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'Port.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'port',
+        'USER': 'brian',
+    'PASSWORD': 'bran23'
     }
 }
 
