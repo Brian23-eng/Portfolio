ef3fe997c5051729c024838a5bcefd7656092119
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-17 16:29:49 +0300

    add models for posting images and instal pillow

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(blank=False, max_length=200)
+    url = models.CharField(blank=False, max_length=200)
+    descrption = models.TextField(blank=False, max_length=500)
+    technology = models.CharField(blank=True, max_length=200)
+    image = models.ImageField(blank=False)
+    
+
+
