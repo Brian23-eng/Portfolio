58b0956a27bf7069d18227319c41900c7ca355cc
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-19 16:19:18 +0300

    change related name in models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Category(models.Model):
-     name: models.CharField(max_length=50)
+     name: models.CharField(blank=True, max_length=50)
      
      
 class Post(models.Model):
