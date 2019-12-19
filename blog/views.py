60361bf4b0acae455bfb9a65d30ea8b284be13d7
brian23-eng <b.odhiambo.bo@gmail.com>
2019-12-19 15:46:01 +0300

    create views for blog and templates and url pattern

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from . models import Comment, Category, Post
+from .forms import CommentForm
 
-# Create your views here.
+def blog_index(request):
+    posts = Post.objects.all().order_by('-creted_on')
+    context = {
+        "posts":posts
+    }
+    return render(request, 'blog.html', context)
+
+
+def blog_category(request, category):
+    posts = Post.objects.filter(categories_name_contains=category).order_by('-created_on')
+    context = {'category':category,
+               'posts':posts
+               
+               }
+    
+    return render(request, 'blog_category.html', context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    forms = CommentForm()
+    if request.form == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author = form.cleaned_data["author"],
+                body = form.cleaned_data["body"],
+                post = post
+            )
+            comment.save()
+    
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form":form,
+    }
+
+    return render(request, "blog_detail.html", context)
